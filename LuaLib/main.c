92e0c0aad1220dd3731c268fda5a43cc98eb9afe
RuiZhuo <339361504@qq.com>
2016-08-13 15:21:20 +0800

    SUMMIT
    SUMMIT

---
+++ LuaLib/main.c
@@ -0,0 +1,101 @@
+//
+//  main.c
+//  LuaLib
+//
+//  Created by 卓锐 on 16/8/4.
+//  Copyright © 2016年 卓锐. All rights reserved.
+//
+
+#include <stdio.h>
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+#include "lobject.h"
+
+int toLuaSum(lua_State *L){
+    if (lua_type(L, 1) == LUA_TNUMBER && lua_type(L, 2) == LUA_TNUMBER) {
+        double a = lua_tonumber(L, 1);
+        double b = lua_tonumber(L, 2);
+        lua_pushnumber(L, (a + b));
+    }
+    else{
+        luaL_error(L, "不是数字");
+    }
+    return 1;
+}
+
+
+//c函数绑定到lua
+void toLuaTest(){
+    lua_State *L = luaL_newstate();
+    //打开所有LUA函数库，这样lua才能调用内置的方法，例如print等
+    luaL_openlibs(L);
+    lua_register(L, "sum", toLuaSum);
+    luaL_dofile(L, "/Users/zhuorui/Documents/lua_test/LuaTest/LuaTest/src/test.lua");
+    lua_close(L);
+}
+
+//入栈，读取测试
+void luaStackTest(){
+    lua_State *L = luaL_newstate();
+    lua_pushstring(L, "test string");
+    lua_pushnumber(L, 123);
+    if (lua_isstring(L, 1)) {
+        printf("%s\n", lua_tostring(L, 1));
+    } 
+    if (lua_isnumber(L, 2)) {
+        printf("%f\n", lua_tonumber(L, 2));
+    }
+    lua_close(L);
+}
+
+void stackDump(lua_State* L){
+    printf("\nbegin dump lua stack");
+    int i = 0;
+    int top = lua_gettop(L);
+    for (i = top; i > 0; --i) {
+        int t = lua_type(L, i);
+        printf("\n");
+        switch (t) {
+            case LUA_TSTRING:
+            {
+                printf("'%s' ", lua_tostring(L, i));
+            }
+                break;
+            case LUA_TBOOLEAN:
+            {
+                printf(lua_toboolean(L, i) ? "true " : "false ");
+            }break;
+            case LUA_TNUMBER:
+            {
+                printf("%g ", lua_tonumber(L, i));
+            }
+                break;
+            default:
+            {
+                printf("%s ", lua_typename(L, t));
+            }
+                break;
+        }
+    }
+    printf("\nend dump lua stack");
+}
+
+void luaTableTest(){
+    lua_State *L = luaL_newstate();
+    lua_newtable(L);
+    lua_pushstring(L, "key1");
+    lua_pushnumber(L, 123);
+    stackDump(L);
+    lua_settable(L, -3);
+    stackDump(L);
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    luaTableTest();
+    
+
+    printf("Hello, World!\n");
+    return 0;
+}
